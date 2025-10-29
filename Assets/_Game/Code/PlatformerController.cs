d3b4850fc71e904233b18d74225bc69d4bb35cc6
Shane Collum <Collum-S@ulster.ac.uk>
2025-10-29 17:15:37 +0000

    Added wall jump
    Added the wall jump, but need to change values around to make it feel better. Added proper code to flip the player. Fixed some code to make it easier to use in the future and more readable.

--- Assets/_Game/Code/PlatformerController.cs
+++ Assets/_Game/Code/PlatformerController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
@@ -17,10 +16,12 @@ public class PlatformerController : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
-    
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
     private Rigidbody2D rb;
-    private bool isGrounded;
     private float moveInput;
+    private bool isFacingRight = true;
     // jump variables
     public float coyoteTime = 0.1f;
     public bool canJump = false;
@@ -28,6 +29,15 @@ public class PlatformerController : MonoBehaviour
     // dash variables
     public bool canDash = true;
     public bool isDashing = false;
+    // wall jump variables
+    private bool isWallSliding;
+    private float wallSlideSpeed = 2f;
+    private bool isWallJumping;
+    private float wallJumpDirection;
+    private float wallJumpTime = 0.2f;
+    private float wallJumpCounter;
+    private float wallJumpDuration = 0.4f;
+    private Vector2 wallJumpPower = new Vector2(8f, 16f);
     
     void Start()
     {
@@ -44,21 +54,28 @@ public class PlatformerController : MonoBehaviour
         // Get horizontal input
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // Check if grounded
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        // calls wall jump functions
+        WallSlide();
+        WallJump();
+
+        // only uses the flip function if player isn't wall jumping
+        if (!isWallJumping)
+        {
+            Flip();
+        }
 
         // coyote time counts down while midair
         coyoteTime -= Time.deltaTime;
 
         // resets values when player is on the ground
-        if (isGrounded)
+        if (IsGrounded())
         {
             coyoteTime = 0.1f;
             canDoubleJump = true;
         }
 
         // allows player to briefly jump if they still have coyote time
-        if (isGrounded || coyoteTime > 0)
+        if (IsGrounded() || coyoteTime > 0)
         {
             canJump = true;
         }
@@ -80,14 +97,14 @@ public class PlatformerController : MonoBehaviour
             canDoubleJump = false;
         }
 
-        // ca only dash on the ground
-        if (Input.GetKeyDown(KeyCode.E) && canDash && isGrounded)
+        // can only dash on the ground
+        if (Input.GetKeyDown(KeyCode.E) && canDash && IsGrounded())
         {
             StartCoroutine(Dash());
         }
 
         // resets dash and speed if player stops moving or lands after dash ends (might try to add turning around as well)
-        if (isGrounded && !isDashing)
+        if (IsGrounded() && !isDashing || IsWalled() && !isDashing)
         {
             moveSpeed = maxSpeed;
             tr.emitting = false;
@@ -102,6 +119,7 @@ public class PlatformerController : MonoBehaviour
         }
     }
 
+    // Coroutine to dash 
     private IEnumerator Dash()
     {
         isDashing = true;
@@ -114,8 +132,11 @@ public class PlatformerController : MonoBehaviour
     
     void FixedUpdate()
     {
-        // Apply horizontal movement
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        // Apply horizontal movement when player isn't wall jumping
+        if (!isWallJumping)
+        {
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        }
         rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
     }
     
@@ -127,5 +148,85 @@ public class PlatformerController : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
+    }
+
+    // flips player on the x axis
+    private void Flip()
+    {
+        if (isFacingRight && moveInput < 0f || !isFacingRight && moveInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    // checks if player is grounded
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+
+    // checks if player is on a wall
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+
+    // lets players slide down walls
+    private void WallSlide()
+    {
+        if (IsWalled() && !IsGrounded() && moveInput != 0f)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    // lets players wall jump if they are wall sliding
+    private void WallJump() // tweak values to feel better to play, far to slow/unresponsive
+        // maybe less time jumping away from wall and no cooldown, akin to hollow knight?
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpDirection = -transform.localScale.x;
+            wallJumpCounter = wallJumpTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.linearVelocity = new Vector2(wallJumpDirection * wallJumpPower.x, wallJumpPower.y);
+            wallJumpCounter = 0f;
+
+            if (transform.localScale.x != wallJumpDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
     }
 }
