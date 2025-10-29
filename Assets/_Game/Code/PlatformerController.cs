3e86f032e9c23943a49117de2371be8824fcac72
Shane Collum <Collum-S@ulster.ac.uk>
2025-10-29 14:14:41 +0000

    added movement back (dash,dj)
    github didnt save last time so I added back the double jump and the dash. Both work perfectly fine. Also added a max linear velocity to make sure the player's speed would be capped when falling.

--- Assets/_Game/Code/PlatformerController.cs
+++ Assets/_Game/Code/PlatformerController.cs
@@ -1,11 +1,18 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float maxSpeed = 7f;
+    [SerializeField] public float maxVelocity = 20f;
     [SerializeField] private float jumpForce = 12f;
-    
+    [SerializeField] private float dashPower = 1.5f;
+    [SerializeField] private float dashTime = 1f;
+    [SerializeField] private TrailRenderer tr;
+
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
@@ -14,8 +21,13 @@ public class PlatformerController : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
     private float moveInput;
+    // jump variables
     public float coyoteTime = 0.1f;
     public bool canJump = false;
+    public bool canDoubleJump = false;
+    // dash variables
+    public bool canDash = true;
+    public bool isDashing = false;
     
     void Start()
     {
@@ -26,21 +38,26 @@ public class PlatformerController : MonoBehaviour
         rb.gravityScale = 3f;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
-    
+
     void Update()
     {
         // Get horizontal input
         moveInput = Input.GetAxisRaw("Horizontal");
-        
+
         // Check if grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // coyote time counts down while midair
         coyoteTime -= Time.deltaTime;
 
+        // resets values when player is on the ground
         if (isGrounded)
         {
             coyoteTime = 0.1f;
+            canDoubleJump = true;
         }
 
+        // allows player to briefly jump if they still have coyote time
         if (isGrounded || coyoteTime > 0)
         {
             canJump = true;
@@ -51,16 +68,55 @@ public class PlatformerController : MonoBehaviour
         }
 
         // Jump input
-        if (Input.GetButtonDown("Jump") && canJump == true)
+        if (Input.GetButtonDown("Jump") && canJump)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        // allows player to double jump midair once
+        if (Input.GetButtonDown("Jump") && !canJump && canDoubleJump)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            canDoubleJump = false;
+        }
+
+        // ca only dash on the ground
+        if (Input.GetKeyDown(KeyCode.E) && canDash && isGrounded)
+        {
+            StartCoroutine(Dash());
+        }
+
+        // resets dash and speed if player stops moving or lands after dash ends (might try to add turning around as well)
+        if (isGrounded && !isDashing)
+        {
+            moveSpeed = maxSpeed;
+            tr.emitting = false;
+            canDash = true;
+        }
+        else if (rb.linearVelocity.magnitude < 5 && isDashing)
+        {
+            moveSpeed = maxSpeed;
+            tr.emitting = false;
+            isDashing = false;
+            canDash = true;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        moveSpeed *= dashPower;
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
     }
     
     void FixedUpdate()
     {
         // Apply horizontal movement
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
     }
     
     // Visualise ground check in editor
