902994c13458a35355bcd78affde7252d1753644
Shane Collum <Collum-S@ulster.ac.uk>
2025-11-04 14:15:45 +0000

    Moving platform and game manager
    Created the moving platforms that will move between any set number of points. Still need to make player move with it and have platforms stop at various points.  Also added the game manager which will help for keeping values between scenes when it gets added

---
+++ Assets/_Game/Code/MovingPlatform.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
+    public float platformStop = 1f;
+
+    private int i;
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                float x = speed;
+                speed = 0;
+                StartCoroutine(Wait());
+                speed = x;
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(platformStop);
+    }
+}
