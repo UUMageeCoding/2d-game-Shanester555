11271ccdfde699ab77003437b4e75f0cb46f40d6
Shane Collum <Collum-S@ulster.ac.uk>
2025-11-07 11:35:48 +0000

    Fixed camera bounds and moving camera
    Camera now stops at appropriate bounds using Cinemachine (may have to download on different computers). Also added to the teleport script to be able to move the camera with the player and set new bounds for the camera, allowing easy room by room creation for the game now. Need to make a transition screen to cover a slight jitter with the camera (which is why the script now waits), which will also need to include figuring out how to disable player controls temporarily.

--- Assets/_Game/Code/TeleportPlayer.cs
+++ Assets/_Game/Code/TeleportPlayer.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
@@ -5,11 +7,29 @@ public class TeleportPlayer : MonoBehaviour
     public GameObject player;
     public Transform target;
 
+    public CinemachineConfiner2D confiner;
+    public Collider2D newBounds;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == ("Player"))
+        if (other.CompareTag("Player"))
         {
-            player.transform.position = target.position;
+            StartCoroutine(TeleportAndChangeBounds());
         }
+    }
+
+    private IEnumerator TeleportAndChangeBounds()
+    {
+        // Teleport the player
+        player.transform.position = target.position;
+
+        // Optionally disable confiner momentarily
+        confiner.BoundingShape2D = null;
+
+        // Wait 1 second (optional, can adjust)
+        yield return new WaitForSeconds(3f);
+
+        // Assign new confiner bounds
+        confiner.BoundingShape2D = newBounds;
     }
 }
