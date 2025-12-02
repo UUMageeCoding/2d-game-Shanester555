5aa2784af7c40f704e0c596eb7811aa6ec1ad340
Shane Collum <Collum-S@ulster.ac.uk>
2025-12-02 13:02:03 +0000

    Finished rooms P and J
    Finished room P which makes the second path to the double jump complete. Also added room J as a one off idea I had. Plan to make room H next to complete the second path to the dash and then work on the right side of the factory outside.

--- Assets/_Game/Code/GameManager.cs
+++ Assets/_Game/Code/GameManager.cs
@@ -24,11 +24,6 @@ public class GameManager : MonoBehaviour
         if (_instance == null)
         {
             _instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
         }
     }
 }
